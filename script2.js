0a18f1f17da0cd574765966198a05aafbe9f1fe5
chris kim <chriskim0926@gmail.com>
2020-10-14 17:27:10 -0400

    added 2nd js file for adding to history

---
+++ script2.js
@@ -0,0 +1,9 @@
+
+
+ $("#searchBtn").on("click", function(event) {
+    var cityHistory = $("#hBtn").val();
+    cityHistory1.push(cityHistory);
+ })
+ 
+ $(document).on("click", ".cityHistoryBtn", seachHistory);
+
